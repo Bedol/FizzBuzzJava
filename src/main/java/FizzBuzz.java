bdc8708ae57cd73119d44b0f28c4d37617ef6073
Bed0l <bartlomiej.weber@gmail.com>
2017-03-28 11:03:51 +0200

    Initial commit for fizzbuzz Kata

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,12 @@
+/**
+ * Created by bweber on 28.03.2017.
+ *
+ */
+public class FizzBuzz {
+    public static String getResult(int i) {
+        if ((i % 15) == 0) return "fizzbuzz";
+        else if ((i % 5) == 0) return "buzz";
+        else if ((i % 3) == 0) return "fizz";
+        return String.valueOf(i);
+    }
+}
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,65 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bweber on 28.03.2017.
+ */
+public class FizzBuzzTest {
+
+    @Test
+    public void returnOneForGivenOne() {
+        assertEquals("1", FizzBuzz.getResult(1));
+    }
+
+    @Test
+    public void returnTwoForGivenTwo() {
+        assertEquals("2", FizzBuzz.getResult(2));
+    }
+
+    @Test
+    public void returnFizzForGivenThree() {
+        assertEquals("fizz", FizzBuzz.getResult(3));
+    }
+
+    @Test
+    public void returnFourForGivenFour() {
+        assertEquals("4", FizzBuzz.getResult(4));
+    }
+
+    @Test
+    public void returnBuzzForGivenFive() {
+        assertEquals("buzz", FizzBuzz.getResult(5));
+    }
+
+    @Test
+    public void returnFizzForGivenSix() {
+        assertEquals("fizz", FizzBuzz.getResult(6));
+    }
+
+    @Test
+    public void returnSevenForGivenSeven() {
+        assertEquals("7", FizzBuzz.getResult(7));
+    }
+
+    @Test
+    public void returnEightForGivenEight() {
+        assertEquals("8", FizzBuzz.getResult(8));
+    }
+
+    @Test
+    public void returnFizzForGivenNine() {
+        assertEquals("fizz", FizzBuzz.getResult(9));
+    }
+
+    @Test
+    public void returnBuzzForGivenTen() {
+        assertEquals("buzz", FizzBuzz.getResult(10));
+    }
+
+    @Test
+    public void returnFizzBuzzForGivenFifteen() {
+        assertEquals("fizzbuzz", FizzBuzz.getResult(15));
+    }
+
+}
